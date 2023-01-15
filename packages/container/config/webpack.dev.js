7d14773b6652c36304400db742d33f986c3f681a
vkotu <vkotu@paypal.com>
2023-01-14 20:45:15 -0800

    created container deploy script

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,5 +1,4 @@
 const { merge } = require("webpack-merge");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const WebpackModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const webpackCommon = require("./webpack.common");
 const packageJson = require("../package.json");
@@ -13,9 +12,6 @@ const devConfig = {
     },
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
-    }),
     new WebpackModuleFederationPlugin({
       name: "container",
       remotes: {
