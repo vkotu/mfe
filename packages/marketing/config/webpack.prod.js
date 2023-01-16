2d5d9385cd270124190d4a2f4d1cca997fe394d5
vkotu <vkotu@paypal.com>
2023-01-15 17:39:59 -0800

    marketing deployments

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -8,7 +8,7 @@ const prodConfig = {
   mode: "production",
   output: {
     filename: "[name].[contenthash].js",
-    publicPath: "/marketing/latest",
+    publicPath: "/marketing/latest/",
   },
   plugins: [
     new ModuleFederationPlugin({
