b52188c1b09f0cf0d5c9a07c4b85054eadccda79
vkotu <vkotu@paypal.com>
2023-01-16 16:11:59 -0800

    add auth package, lazy loadin etc

--- packages/auth/config/webpack.prod.js
+++ packages/auth/config/webpack.prod.js
@@ -12,7 +12,7 @@ const prodConfig = {
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: "marketing",
+      name: "auth",
       filename: "remoteEntry.js",
       exposes: {
         "./AuthApp": "./src/bootstrap",
